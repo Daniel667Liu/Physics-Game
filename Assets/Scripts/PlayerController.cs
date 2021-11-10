afd5f88cc1d8f11a34082a9a8f34373d3a57d319
Yuri <yw5939@nyu.edu>
2021-11-09 20:44:40 -0500

    Player Controller Test 0.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,13 @@ public class PlayerController : MonoBehaviour
     float InputZ;
     Vector3 v_movement;
     Vector3 v_velocity;
-    float moveSpeed;
-    float gravity;
+    [Header("Movement Data")]
+    public float moveSpeed;
+    public float gravity;
+    public float jumpForce;
+    public float rotateSpeed;
     void Start()
     {
-        moveSpeed = 4f;
-        gravity = 0.5f;
     }
 
     // Update is called once per frame
@@ -39,15 +40,19 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)&& charController.isGrounded)
         {
             //Apply a force to this Rigidbody in direction of this GameObjects up axis
-            v_velocity.y += 0.1f;
+            v_velocity.y += jumpForce;
         }
 
-        InputX = Input.GetAxis("Horizontal");
-        InputZ = Input.GetAxis("Vertical");
+        if (charController.isGrounded)
+        {
+            InputX = Input.GetAxis("Horizontal");
+            InputZ = Input.GetAxis("Vertical");
+            
+        }
 
         v_movement = charController.transform.forward * InputZ;
 
-        charController.transform.Rotate(Vector3.up * InputX * (100f * Time.deltaTime));
+        charController.transform.Rotate(Vector3.up * InputX * (rotateSpeed * Time.deltaTime));
 
         charController.Move(v_movement * moveSpeed * Time.deltaTime);
         charController.Move(v_velocity);
