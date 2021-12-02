172b498333e6e97e4230803a6532b6de737577f3
Daniel <daniel67liu@gmail.com>
2021-12-02 01:02:50 -0500

    Joystick control and vibration added, in scene "PG_Daniel4"

---
+++ Assets/Scripts/ControllerVibration.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure; // Required in C#
+
+public class ControllerVibration : MonoBehaviour
+{
+   //define variables using in controller vibration
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    public ShipCollider SpaceshipColliderHolder;
+    //the holder should be a gameobject contains only the colliders and public bool
+    private bool IsHitted;
+    public float VibrateStrength = 1f;
+
+
+
+    
+
+    public void SpeedVibration() 
+    {
+        float SpeedVibStrength = Input.GetAxis("Forward") * 0.05f;
+        GamePad.SetVibration(playerIndex, SpeedVibStrength, SpeedVibStrength);
+    }
+    
+    void Start()
+    {
+        SpaceshipColliderHolder = FindObjectOfType<ShipCollider>();
+        //need to add a WHOLE collider to the spaceship.
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        IsHitted = SpaceshipColliderHolder.isHitted;//detect if the ship is hitted from the holder
+        if (IsHitted)
+        {
+            GamePad.SetVibration(playerIndex, VibrateStrength, VibrateStrength);
+        }
+
+        else 
+        {
+            GamePad.SetVibration(playerIndex, 0f, 0f);
+        }
+        //Debug.Log(IsHitted);
+    }
+}
