b923a6a02a3c693d84b4ec4c9a71cf644fe07b31
Yuri <yw5939@nyu.edu>
2021-12-02 17:05:30 -0500

    Level v0.2

--- Assets/Scripts/P_SelfRotate.cs
+++ Assets/Scripts/P_SelfRotate.cs
@@ -7,6 +7,7 @@ public class P_SelfRotate : MonoBehaviour
     // Start is called before the first frame update
     public GameObject Planet;
     public float speedX, speedY, speedZ;
+    bool rotate = true;
     void Start()
     {
         
@@ -15,6 +16,15 @@ public class P_SelfRotate : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(true)
         this.transform.Rotate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedZ * Time.deltaTime, Space.Self);
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            rotate = false;
+        }
     }
 }
