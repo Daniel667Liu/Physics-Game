fb585dc0955af3e658a3e8501e7a479e99563584
Yuri <yw5939@nyu.edu>
2021-12-09 11:47:33 -0500

    exchange bio planet

--- Assets/Scripts/SpaceShip/MissionPoints.cs
+++ Assets/Scripts/SpaceShip/MissionPoints.cs
@@ -10,30 +10,36 @@ public class MissionPoints : MonoBehaviour
     public Transform PlanetTest;
     public Transform [] Planets;
     public TMP_Text Dist;
+
+    public Text meter;
+    public Vector3 offset;
+
+
     //
     public int MissionIndex = 0;
     void Start()
     {
 
-    private void Update()
+    void Update()
     {
         // Giving limits to the icon so it sticks on the screen
         // Below calculations witht the assumption that the icon anchor point is in the middle
         // Minimum X position: half of the icon width
-        float minX = img.GetPixelAdjustedRect().width / 2;
-        // Maximum X position: screen width - half of the icon width
+        float minX = Point.GetPixelAdjustedRect().width / 2;
+            // Maximum X position: screen width - half of the icon width
+            Debug.Log(minX);
         float maxX = Screen.width - minX;
-
         // Minimum Y position: half of the height
-        float minY = img.GetPixelAdjustedRect().height / 2;
-        // Maximum Y position: screen height - half of the icon height
-        float maxY = Screen.height - minY;
+        float minY = Point.GetPixelAdjustedRect().height / 2;
+            Debug.Log(minY);
+            // Maximum Y position: screen height - half of the icon height
+            float maxY = Screen.height - minY;
 
         // Temporary variable to store the converted position from 3D world point to 2D screen point
-        Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offset);
+        Vector2 pos = Camera.main.WorldToScreenPoint(PlanetTest.position);
 
         // Check if the target is behind us, to only show the icon once the target is in front
-        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        if (Vector3.Dot((PlanetTest.position - transform.position), transform.forward) < 0)
         {
             // Check if the target is on the left side of the screen
             if (pos.x < Screen.width / 2)
@@ -52,10 +58,10 @@ public class MissionPoints : MonoBehaviour
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
-        // Update the marker's position
-        img.transform.position = pos;
+            // Update the marker's position
+            Point.transform.position = pos;
         // Change the meter text to the distance with the meter unit 'm'
-        meter.text = ((int)Vector3.Distance(target.position, transform.position)).ToString() + "m";
+        meter.text = ((int)Vector3.Distance(PlanetTest.position, transform.position)).ToString() + "m";
     }
 
         Dist.text = Vector3.Distance(this.transform.position, Planets[MissionIndex].position).ToString();
